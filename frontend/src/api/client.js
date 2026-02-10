0bb3fa33fc4cf88430e4428a5ebb34c5b58e8ab6
Sarath G <gunasarath05@gmail.com>
2026-02-10 17:32:20 +0530

    Update API base URL to production server

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
+//const API_BASE_URL = 'http://127.0.0.1:8000/api/';
+const API_BASE_URL = 'https://jishnu-clinic-v1.onrender.com';
+
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
